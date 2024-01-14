23a8193ecb290cc7111d199d77efec9f9702fe3b
krunoslavp <krunoslav.petrovic@prodyna.com>
2024-01-14 15:08:30 +0100

    set allow cors on repositories

--- src/main/java/com/chevronsix/restmockserver/repositories/AuthorRepository.java
+++ src/main/java/com/chevronsix/restmockserver/repositories/AuthorRepository.java
@@ -3,7 +3,9 @@ package com.chevronsix.restmockserver.repositories;
 import com.chevronsix.restmockserver.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 }
--- src/main/java/com/chevronsix/restmockserver/repositories/PublicationRepository.java
+++ src/main/java/com/chevronsix/restmockserver/repositories/PublicationRepository.java
@@ -3,7 +3,9 @@ package com.chevronsix.restmockserver.repositories;
 import com.chevronsix.restmockserver.models.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 public interface PublicationRepository extends JpaRepository<Publication, Integer> {
 }
